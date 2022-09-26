dfd97460fe3791085b53d2fd54d454f322bd6c9c
robinsacher <110891559+robinsacher@users.noreply.github.com>
2022-09-26 15:41:06 +0200

    Add files via upload

---
+++ Hypothekenrechner/Hypothekenrechner/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Hypothekenrechner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                  
+            Console.WriteLine("Hypothekenrechner");
+            Console.WriteLine("");
+            
+            Console.Write("Bitte geben sie den Betrag der Hypothek ein:");
+            double Hypothek = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Geben sie den Zinssatz ein:");
+            double zinssatz = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+        }
+    }
+}
